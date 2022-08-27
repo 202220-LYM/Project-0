e1882de0153ecffc66c477da6251838302b839d9
Lina-go <77931393+Lina-go@users.noreply.github.com>
2022-08-26 20:56:20 -0500

    commit

---
+++ P0/P0 Lym.py
@@ -0,0 +1,120 @@
+#P0 Lym
+
+
+# M: to move forward
+# R: to turn right
+# C: to drop a chip
+# B: to place a balloon
+# c: to pickup a chip
+# b: to grab a balloon
+# P: to pop a balloon
+# J(n): to jump forward n steps. It may jump over obstacles, but the final postion sould not have an obstacle.
+# G(x,y): to go to position (x,y). Position (x,y) should not have an obstacle.
+# class uniandes.lym.robot.kernel. RootWorldDec
+
+
+# A program defintion begins with the keyword PROG
+# It ends the keyword GORP
+# A declaration of variables is the keyword VAR followed by a list of names separated by commas.
+# A name is a string of alphanumeric characters that begins with a letter
+# The list is followed by ;
+# A procedure defintion is a the word PROC followed by a name followed by a list of parameters within parenthesis separated by commas, 
+#  followed by a block of instructions and ending with the word CORP.
+# A block of instructions is a sequence of instructions separated by semicolons within curly brackets.
+# An instruction can be a command,a control structure or a procedure call.
+# Spaces, newlines, and tabulators are separators and should be ignored.
+
+# Command:
+    # An assignment: name := n where name is a variable name and n is a number. The result of this instruction is to assign the value of the number to the variable.
+    # walk(n) – where n is a number or a variable or a parameter. The robot should move n steps forward.
+    # jump(n) – where n is a number or a variable or a parameter. The robot should jump n steps forward.
+    # jumpTo(n,m) – where n and m are numbers, variables, or parameters. The robot should jump to position (n,m).
+    # veer(D) – where D can be left, right, or around. The robot should turn 90 degrees in the direction D.
+    # look(O) – where O can be north, south, east or west. The robot should turn so that it ends up facing direction O.
+    # drop(n) – where n is a number or a variable or a parameter. The Robot should put n chips from its position.
+    # grab(n) – where n is a number or a variable or a parameter. The Robot should get n ballons from its position.
+    # get(n) – where n is a number or a variable or a parameter. The Robot should get n chips from its position.
+    # free(n) – where n is a number or a variable or a parameter. The Robot should put n balloons from its position.
+    # pop(n) – where n is a number or a variable or a parameter. The Robot should pop n balloons from its position.
+    # walk(d,n) – where n is a number or a variable or a parameter; d is a direction, either front, right, left, back. The robot should move n positions to the front, to the left, the right or back and end up facing the same direction as it started.
+    # walk(o,n) – where n is a number or a variable or a parameter; o is north, south, west, or east. The robot should face O and then move n steps.
+
+# Control Structure:
+#     Conditional: if (condition)Block1 else Block2 fi – Executes Block1 if condition is true and Block2 if condition is false.
+#     Conditional if (condition)Block1 fi– Executes Block1 if condition is true does not do anything if it is false.
+#     Loop: while (condition)do Block od – Executes Block while condition is true.
+#     Repeat: repeatTimes n Block per – Executes Block n times, where n is a variable or a parameter or a number.
+
+# Conditions:
+    # isfacing(O) – where O is one of: north, south, east, or west
+    # isValid(ins,n) – where ins can be walk, jump, grab, pop, pick, free, drop and n is a number or a variable. It is true if ins(n) can be executed without error.
+    # canWalk(d,n) – where D is one of: north, south, east, or west and n is a number, a variable or a parameter.
+    # canWalk(o,n) – where D is one of: right, left, front, or back and n is a number, a variable or a parameter.
+    # not (cond) – where cond is a condition
+
+# Implement a simple yes/no parser.
+# Verify whether the syntax is correct.
+# You must verify that used function names and variable names have been previously defined or in the case of functions, that they are the function’s arguments.
+# allow recursion
+
+# archivo= open("texto.txt", 'r')
+
+
+from curses.ascii import isspace
+import sys
+
+variables = []
+
+
+with open("texto.txt", 'r') as f:
+        lines = f.readlines()
+        print(lines)
+
+def proc(linea):
+
+    pass
+
+def var(linea):
+    linea.delete("VAR")
+    if linea[-1]!=";":
+        print("NO")
+        sys.exit()
+    varia=linea.split(",")
+    if linea.count(",") != len(varia)-1:
+        print("NO")
+        sys.exit() 
+    return varia
+
+
+
+
+
+#Revisa primera linea de programa
+if lines[0]!="PROG\n":
+    print(lines[0])
+    print("NO")
+    sys.exit()
+
+for l in lines[1:-1]:
+
+    if str(l).isspace()==False :
+        if l.startswith("VAR"):
+           variables= var(l)
+    
+
+        
+
+
+
+#Revisa ultima liena del programa
+if lines[-1]!="GORP":
+    print(lines[-1])
+    print("NO\n")
+    sys.exit()
+
+
+
+# for line in archivo:
+#     print(line)
+
+
