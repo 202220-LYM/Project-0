89cd687506d6295faf85c77e7b5d4a06921aecd2
Lina-go <77931393+Lina-go@users.noreply.github.com>
2022-08-29 20:05:24 -0500

    Comaa

--- P0/P0LymFinal.py
+++ P0/P0LymFinal.py
@@ -245,20 +245,20 @@ def comando(variables_loc, stringLista):
     for j in stringLista:
         if "(" in j and ")" in j:
             proc = j.replace(" ", "").split("(")
-            if proc[0] == "walk": # Si el comando es walk se ejecuta la función walk
+            if proc[0] == "walk":  # Si el comando es walk se ejecuta la función walk
                 walk(j, variables_loc)
-            elif proc[0] == "jump" or proc[0] == "drop" or proc[0] == "grab" or proc[0] == "get" or proc[0] == "free": # Si el comando es jump, drop, grab, get o free se ejecuta la función caso1
+            elif proc[0] == "jump" or proc[0] == "drop" or proc[0] == "grab" or proc[0] == "get" or proc[0] == "free":  # Si el comando es jump, drop, grab, get o free se ejecuta la función caso1
                 caso1(j, variables_loc)
             elif proc[0] == "jumpTo":
-                caso2(j, variables_loc) # Si el comando es jumpTo se ejecuta la función caso2
+                caso2(j, variables_loc)  # Si el comando es jumpTo se ejecuta la función caso2
             elif proc[0] == "veer" or proc[0] == "look":
-                caso3(j)                # Si el comando es veer o look se ejecuta la función caso3
+                caso3(j)  # Si el comando es veer o look se ejecuta la función caso3
             elif proc[0] == "if" or proc[0] == "while" or j.startswith("repeatTimes"):
-                conStru(j, variables_loc) # Si el comando es if, while o repeatTimes se ejecuta la función conStru
+                conStru(j, variables_loc)  # Si el comando es if, while o repeatTimes se ejecuta la función conStru
             elif proc[0] in dic_PROC.keys():
-                dicProc(j.removeprefix(proc[0]), proc[0]) # Si el comando es una función de procesamiento se ejecuta la función dicProc
+                dicProc(j.removeprefix(proc[0]), proc[0])  # Si el comando es una función de procesamiento se ejecuta la función dicProc
             else:
-                abort() # Si no se cumple ninguna condición aborta el programa
+                abort()  # Si no se cumple ninguna condición aborta el programa
         else:  # revisar si es una asignacion
             asig(j)
 
@@ -266,92 +266,95 @@ def comando(variables_loc, stringLista):
 def asig(j):
     """ :param j: 1 línea del archivo .txt """
     dummy = False
-    for v in variables_globales: # Se comprueba que la variable no esté en variables globales
-        if j.split(":")[0] == v: # Si la variable está en variables globales se convierte a dummy=True
+    for v in variables_globales:  # Se comprueba que la variable no esté en variables globales
+        if j.split(":")[0] == v:  # Si la variable está en variables globales se convierte a dummy=True
             dummy = True
-    if dummy is False:          # Si dummy es False aborta
+    if dummy is False:  # Si dummy es False aborta
         abort()
     else:
-        var1 = j.split(":=")    # De lo contrario se separa por :=
+        var1 = j.split(":=")  # De lo contrario se separa por :=
         if len(var1) == 1 or var1[1].isnumeric() is False:
-            abort()             # Si no se cumple la condición aborta el programa
+            abort()  # Si no se cumple la condición aborta el programa
 
 
 def dicProc(lin, nom):
     """ :param lin: 1 línea del archivo .txt :param nom: nombre del comando """
     num = dic_PROC[nom]
     if len(lin.split(",")) != num:  # Se comprueba que tenga el número de variables correcto
-        abort()                     # Si no se cumple la condición aborta el programa
+        abort()  # Si no se cumple la condición aborta el programa
     else:
         lini = lin.removeprefix("(").removesuffix(")").replace(" ", "").split(",")  # Se quita los paréntesis y se separa por ,
         for l in lini:
-            if not l.isnumeric():   # Se comprueba que todas las variables sean numéricas
-                abort()             # Si no se cumple la condición aborta el programa
+            if not l.isnumeric():  # Se comprueba que todas las variables sean numéricas
+                abort()  # Si no se cumple la condición aborta el programa
 
 
 # definición de walk
 def walk(linea, lista):
     """ :param linea: 1 línea del archivo .txt :param lista: lista """
     var = linea.split("(")
-    par = var[1].strip(")").replace(" ", "").split(",") # Se quita los paréntesis y se separa por ,
+    par = var[1].strip(")").replace(" ", "").split(",")  # Se quita los paréntesis y se separa por ,
     if len(var[1].split(",")) == 1:
-        caso1(linea, lista)                             # Si solo tiene 1 var se ejecuta la función caso1
+        caso1(linea, lista)  # Si solo tiene 1 var se ejecuta la función caso1
     elif len(var[1].split(",")) == 2:
         if par[0] not in direcciones:
             if par[0] not in orientaciones:
-                abort()                                 # Si la primera var no es una dirección o una orientación aborta el programa
-        if not par[1].isnumeric():                      # no es un número se verifica que es variable
-            isVar(lista, par[1])                        # Si es variable se ejecuta la función isVar
+                abort()  # Si la primera var no es una dirección o una orientación aborta el programa
+        if not par[1].isnumeric():  # no es un número se verifica que es variable
+            isVar(lista, par[1])  # Si es variable se ejecuta la función isVar
     elif len(var[1].split(",")) > 2:
-        abort()                                         # Si tiene mas de 2 variables aborta el programa
+        abort()  # Si tiene mas de 2 variables aborta el programa
+
 
 # definición de valido
 def valido(linea, lista):
     """ :param linea: 1 línea del archivo .txt :param lista: lista """
-    if "(" in linea and ")" in linea:                   # Se comprueba que tenga paréntesis
+    if "(" in linea and ")" in linea:  # Se comprueba que tenga paréntesis
         var = linea.replace(" ", "").split("(")
-        paras = var[1].strip(")").split(",")            # Se separa por ,
+        paras = var[1].strip(")").split(",")  # Se separa por ,
 
-        if not len(var[1].split(",")) == 2:             # Se comprueba que tenga 2 variables
-            abort()                                     # Si no se cumple la condición aborta el programa
+        if not len(var[1].split(",")) == 2:  # Se comprueba que tenga 2 variables
+            abort()  # Si no se cumple la condición aborta el programa
         else:
             if paras[0] not in ins:
-                abort()                                 # Si la primera variable no es una instrucción aborta el programa
-            elif not paras[1].isnumeric():              # no es un número se verifica que es variable (nombre o variable)
-                isVar(lista, paras[1])                  # Si es variable se ejecuta la función isVar
+                abort()  # Si la primera variable no es una instrucción aborta el programa
+            elif not paras[1].isnumeric():  # no es un número se verifica que es variable (nombre o variable)
+                isVar(lista, paras[1])  # Si es variable se ejecuta la función isVar
     else:
-        abort()                                         # Si no tiene paréntesis aborta el programa
+        abort()  # Si no tiene paréntesis aborta el programa
+
 
 # Definición del bloque de PROC
 def proc(bloque):
     """ :param bloque:bloque del archivo .txt """
-    variables_loc = primeralproc(bloque[0])             # Se analiza la primera línea de PROC
+    variables_loc = primeralproc(bloque[0])  # Se analiza la primera línea de PROC
     bloque.pop(0)
-    bloque.pop(-1)                                      # Se eliminan PROC y CORP
+    bloque.pop(-1)  # Se eliminan PROC y CORP
     string_bloque = ""
     for i in bloque:
-        string_bloque += i                              # Se concatena cada línea del bloque
+        string_bloque += i  # Se concatena cada línea del bloque
     sb = string_bloque.replace(" ", "").replace("\n", "")
-    if sb.startswith("{") is False or sb.endswith("}") is False: # Se comprueba que el bloque empiece y termine con { y }
-        abort()                                                  # Si no se cumple la condición aborta el programa
+    if sb.startswith("{") is False or sb.endswith("}") is False:  # Se comprueba que el bloque empiece y termine con { y }
+        abort()  # Si no se cumple la condición aborta el programa
     stringLista = sb.strip("{}").split(";")
-    comando(variables_loc, stringLista)                 # Se ejecuta la función comando
+    comando(variables_loc, stringLista)  # Se ejecuta la función comando
+
 
 # Definición del bloque final
 def bloque(bloq):
     """ :param bloq:bloque del archivo .txt """
     string_bloque = ""
     for i in bloq:
-        string_bloque += i                            # Se concatena cada línea del bloque
+        string_bloque += i  # Se concatena cada línea del bloque
     sb = string_bloque.replace(" ", "").replace("\n", "")  # Se quita los espacios y los saltos de línea
     if sb.startswith("{") is False or sb.endswith("}") is False:
-        abort()                                       # Si no se cumple la condición aborta el programa
+        abort()  # Si no se cumple la condición aborta el programa
     else:
-        stringLista = sb.strip("{}").split(";")       # Se separa por ; y se borran {}
-        if sb.count(";") != len(stringLista) - 1:     # Se comprueba que haya tantos ; como líneas
-            abort()                                   # Si no se cumple la condición aborta el programa
+        stringLista = sb.strip("{}").split(";")  # Se separa por ; y se borran {}
+        if sb.count(";") != len(stringLista) - 1:  # Se comprueba que haya tantos ; como líneas
+            abort()  # Si no se cumple la condición aborta el programa
         else:
-            comando(None, stringLista)                # Se ejecuta la función comando
+            comando(None, stringLista)  # Se ejecuta la función comando
 
 
 # verifica si es una variable del programa
@@ -373,74 +376,82 @@ def var(linea):
 def alphaNum(lista):
     """ :param lista: lista """
     for f in lista:
-        if f.isalnum() is False or f[0].isalpha() is False: # si no es alfanumérico o no es una letra
-            abort()                                         # aborta
+        if f.isalnum() is False or f[0].isalpha() is False:  # si no es alfanumérico o no es una letra
+            abort()  # aborta
 
 
 ##############################################
 """PROGRAMA PRINCIPAL"""
+
+
 ##############################################
+def principal(archivo_tx):
+    with open(archivo_tx, 'r') as f:  # se abre el archivo con el nombre texto.txt
+        lines = f.readlines()  # se lee el archivo y se guarda en una lista
 
-with open("prueba.txt", 'r') as f:  # se abre el archivo con el nombre texto.txt
-    lines = f.readlines()  # se lee el archivo y se guarda en una lista
+    string_bloque1 = ""
+    for i in lines:
+        string_bloque1 += i  # Se concatena cada línea del archivo
+    string_bloque = string_bloque1.replace(" ", "").replace("\n", "")  # Se quita los espacios y los saltos de línea
 
-string_bloque1 = ""
-for i in lines:
-    string_bloque1 += i                           # Se concatena cada línea del archivo
-string_bloque = string_bloque1.replace(" ", "").replace("\n", "") # Se quita los espacios y los saltos de línea
-
-if string_bloque.count("{") % 2 == 0 or string_bloque.count("}") % 2 == 0 or string_bloque.count("(") % 2 == 0 or string_bloque.count(")") % 2 == 0:
-    # Revisa primera linea de programa
-    if string_bloque.startswith("PROG") and string_bloque.endswith("GORP"):  # si la primera linea no es PROG se termina el programa
-        i = 1
-        while i < len(lines[1:-1]):  # Se salta la primera linea y última linea del archivo
-            """PARTE A: VAR"""
-            l = lines[i].replace("\n", "").replace(" ", "")
-            if not l == "":  # si la linea no es un espacio se revisa si es una declaración de variables
-                if l.startswith("VAR"):  # si la linea inicia con VAR se revisa si es una declaración de variables
-                    variables_globales.extend(var(l))  # se guardan las variables en una lista
-                elif l.startswith("PROC"): # si la linea inicia con PROC se revisa si es una declaración de PROC
-                    indexProc = i      # se guarda el índice de la línea de PROC
-                    indexCorp = -1     # se guarda el índice de CORP
-                    dummy = False
-                    j = indexProc + 1 # se actualiza el índice de la línea
-                    while j < len(lines[1:-1]) and dummy is False:
-                        t = lines[j].replace("\n", "").replace(" ", "")
-                        if t.startswith("PROC"): # si la línea inicia con PROC
-                            break                # se termina el ciclo
-                        elif t.startswith("CORP"):
-                            dummy = True         # se actualiza el índice de CORP
-                            indexCorp = j
-                        j += 1
-                    if dummy:
-                        proc(lines[indexProc:indexCorp + 1])
+    if string_bloque.count("{") % 2 == 0 or string_bloque.count("}") % 2 == 0 or string_bloque.count("(") % 2 == 0 or string_bloque.count(")") % 2 == 0:
+        # Revisa primera linea de programa
+        if string_bloque.startswith("PROG") and string_bloque.endswith("GORP"):  # si la primera linea no es PROG se termina el programa
+            i = 1
+            while i < len(lines[1:-1]):  # Se salta la primera linea y última linea del archivo
+                """PARTE A: VAR"""
+                l = lines[i].replace("\n", "").replace(" ", "")
+                if not l == "":  # si la linea no es un espacio se revisa si es una declaración de variables
+                    if l.startswith("VAR"):  # si la linea inicia con VAR se revisa si es una declaración de variables
+                        variables_globales.extend(var(l))  # se guardan las variables en una lista
+                    elif l.startswith("PROC"):  # si la linea inicia con PROC se revisa si es una declaración de PROC
+                        indexProc = i  # se guarda el índice de la línea de PROC
+                        indexCorp = -1  # se guarda el índice de CORP
+                        dummy = False
+                        j = indexProc + 1  # se actualiza el índice de la línea
+                        while j < len(lines[1:-1]) and dummy is False:
+                            t = lines[j].replace("\n", "").replace(" ", "")
+                            if t.startswith("PROC"):  # si la línea inicia con PROC
+                                break  # se termina el ciclo
+                            elif t.startswith("CORP"):
+                                dummy = True  # se actualiza el índice de CORP
+                                indexCorp = j
+                            j += 1
+                        if dummy:
+                            proc(lines[indexProc:indexCorp + 1])
+                        else:
+                            abort()
+                        i = indexCorp
+                    elif l.startswith("{"):
+                        indexcorchi = i  # se guarda el índice de la línea de { del corchete de inicio
+                        indexcorchf = -1  # se guarda el índice de la línea de } del corchete de fin
+                        dummy = False
+                        j = indexcorchi + 1
+                        while j < len(lines[1:-1]) and dummy is False:  # meintras que el índice de la línea no sea mayor que el tamaño de la lista y el dummy sea falso
+                            t = lines[j].replace("\n", "").replace(" ", "")  # se quita los espacios y los saltos de línea
+                            if t.startswith("{"):  # si la línea inicia con {
+                                break  # se termina el ciclo
+                            elif t.startswith("}"):  # si la línea inicia con }
+                                dummy = True  # se actualiza el índice de }
+                                indexcorchf = j
+                            j += 1
+                        if dummy:
+                            bloque(lines[indexcorchi:indexcorchf + 1])  # se ejecuta la función bloque
+                        else:
+                            abort()  # si no se cumple la condición aborta el programa
+                        i = indexcorchf
                     else:
-                        abort()
-                    i = indexCorp
-                elif l.startswith("{"):
-                    indexcorchi = i    # se guarda el índice de la línea de { del corchete de inicio
-                    indexcorchf = -1   # se guarda el índice de la línea de } del corchete de fin
-                    dummy = False
-                    j = indexcorchi + 1
-                    while j < len(lines[1:-1]) and dummy is False: # meintras que el índice de la línea no sea mayor que el tamaño de la lista y el dummy sea falso
-                        t = lines[j].replace("\n", "").replace(" ", "") # se quita los espacios y los saltos de línea
-                        if t.startswith("{"):                # si la línea inicia con {
-                            break                           # se termina el ciclo
-                        elif t.startswith("}"):            # si la línea inicia con }
-                            dummy = True                  # se actualiza el índice de }
-                            indexcorchf = j
-                        j += 1
-                    if dummy:
-                        bloque(lines[indexcorchi:indexcorchf + 1]) # se ejecuta la función bloque
-                    else:
-                        abort()                                   # si no se cumple la condición aborta el programa
-                    i = indexcorchf
-                else:
-                    abort()                                      # si no se cumple la condición aborta el programa
-            i += 1                                               # se actualiza el índice de la línea
+                        abort()  # si no se cumple la condición aborta el programa
+                i += 1  # se actualiza el índice de la línea
+        else:
+            abort()  # si no se cumple la condición aborta el programa
     else:
-        abort()                                                  # si no se cumple la condición aborta el programa
-else:
-    abort()                                                      # si no se cumple la condición aborta el programa
+        abort()  # si no se cumple la condición aborta el programa
 
-print("SI")                                                      # Si el programa está bien escrito imprime SI
+    print("SI")  # Si el programa está bien escrito imprime SI
+
+
+"""LUGAR DE LA EJECUCIÓN DE PROGRAMA PRINCIPAL"""
+if __name__ == '__main__':
+    nombre_archivo = "prueba.txt"
+    principal(nombre_archivo)  # se ejecuta el programa principal
